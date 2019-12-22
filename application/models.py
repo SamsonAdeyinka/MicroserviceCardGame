015a457183ab63130aa04673fe34dc77a35791b4
SamsonAdeyinka <samsonadeyinka@hotmail.co.uk>
2019-12-22 22:35:40 +0000

    Deck of cards database

---
+++ application/models.py
@@ -0,0 +1,12 @@
+from application import db
+
+#Database for the deck of cards.
+
+class Deck(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    card_shape = db.Column(db.String(7), nullable=False)
+    card_number = db.Column(db.String(5), nullable=False)
+    card_colour = db.Column(db.String(5), nullable=False)
+
+    def __repr__(self):
+        return ''.join(['card:', self.card_number, 'of', self.card_shape])
