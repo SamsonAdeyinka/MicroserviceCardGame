28f8cdf4ebb8d2e8b65e189d0377644e2fa9c6ae
SamsonAdeyinka <samsonadeyinka@hotmail.co.uk>
2019-12-27 19:20:18 +0000

    Started the routes for the homepage and page 1

---
+++ application/routes.py
@@ -0,0 +1,28 @@
+from flask import render_template, redirect, request
+application import app, db
+application.models import Decks
+application.forms import StartGame, CardColour, DrawCard
+
+@app.route('/')
+
+@app.route('/home', method=['GET', 'POST'])
+def home():
+    form = StartGame()
+    if request.method == ['POST']:
+        return render_template('page-1.html', title='Page 1')
+    return render_template('home.html', title='Home')
+
+@app.route('/page-1', method=['GET', 'POST'])
+def card():
+    form = CardColour():
+    if request.method == 'POST':
+        if request.form == 'black_sub':
+            return render_template('page-2.html', tite='Page 2')
+        elif request.form == 'red_sub':
+            return render_template('page-2.html', title='Page 2')
+    return render_template('page-1.html', tite='Page 1')
+
+@app.route('page-2', method=['GET', 'POST'])
+def draw():
+    form = DrawCard()
+    
