26e23e1272c912bf1cef6ab65112097ec1bfdf96
SamsonAdeyinka <samsonadeyinka@hotmail.co.uk>
2019-12-27 19:18:57 +0000

    Added database IP

--- application/__init__.py
+++ application/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@.........../cards_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@35.242.157.199/cards_db'
 
 db = SQLAlchemy(app)
 
