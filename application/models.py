1a94aa25dd2e50eb4d2b019f3c423696dde01405
SamsonAdeyinka <samsonadeyinka@hotmail.co.uk>
2019-12-31 02:15:20 +0000

    added description and card points

--- application/models.py
+++ application/models.py
@@ -4,9 +4,14 @@ from application import db
 
 class Deck(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    card_shape = db.Column(db.String(7), nullable=False)
-    card_number = db.Column(db.String(5), nullable=False)
-    card_colour = db.Column(db.String(5), nullable=False)
+    Colour = db.Column(db.String(7), nullable=False)
+    Shape = db.Column(db.String(5), nullable=False)
+    Number = db.Column(db.String(5), nullable=False)
+    Description = db.Column(db.String(20), nullable=False)
+    Points = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
-        return ''.join(['card:', self.card_number, 'of', self.card_shape])
+        return ''.join([
+            'card:', self.Description, '\r\n',
+            'points:', self.Points
+            ])
