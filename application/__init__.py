a420a279c91130ee3a97a0aec19952f9196a5a91
SamsonAdeyinka <samsonadeyinka@hotmail.co.uk>
2020-01-01 17:23:49 +0000

    SECRET KEY

--- application/__init__.py
+++ application/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@35.242.157.199/cards_db'
+app.config['SECRET_KEY'] = 'Z2XW3CE45VRBT6U8MOP'
 
 db = SQLAlchemy(app)
 
