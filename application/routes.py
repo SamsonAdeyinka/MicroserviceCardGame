57c6e149ab23ff6861e59d6c810f5cf8b2df4fe7
SamsonAdeyinka <samsonadeyinka@hotmail.co.uk>
2020-01-04 00:31:26 +0000

    define form

--- application/routes.py
+++ application/routes.py
@@ -10,7 +10,7 @@ def home():
     form = StartGame()
     #if request.method == ['POST']:
         #return render_template('page-1.html', title='Page 1')
-    return render_template('home.html', title='Home')
+    return render_template('home.html', title='Home', form=form)
 
 @app.route('/page-1', methods=['GET', 'POST'])
 def card():
@@ -20,14 +20,14 @@ def card():
             return render_template('page-2.html', tite='Page 2')
         elif request.form == 'red_sub':
             return render_template('page-2.html', title='Page 2')
-    return render_template('page-1.html', tite='Page 1')
+    return render_template('page-1.html', tite='Page 1', form=form)
 
 @app.route('/page-2', methods=['GET', 'POST'])
 def draw():
     form = DrawCard()
     if request.form == 'POST':
-        return render_template('page-3', title='Page 3')
+        return render_template('page-3', title='Page 3', form=form)
 
 @app.route('/page-3')
 def prize():
-    return render_template('page-3.html', title='Page 3')
+    return render_template('page-3.html', title='Page 3', form=form)
