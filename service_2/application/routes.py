102c046c3b1e2bddd6541a938299aec0f2ddd846
SamsonAdeyinka <samsonadeyinka@hotmail.co.uk>
2020-01-11 14:50:10 +0000

    db query error fix

--- service_2/application/routes.py
+++ service_2/application/routes.py
@@ -14,10 +14,10 @@ number = random_int()
 @app.route('/service_2', methods=['POST'])
 def card_points():
     query = Deck.query.filter_by(id=number).first()
-    point = query.points
+    point = query.Points
     return {'points':'{}'.format(point)}
 
 def card():
     query = Deck.query.filter_by(id=number).fisrt()
-    card = query.card
+    card = query.Description
     return {'card':'{}'.format(card)}
