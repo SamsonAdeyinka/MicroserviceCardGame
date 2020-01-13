0d528f170bb1cf7efc9bda0560f3fbb4632f7531
Samson Adeyinka <samsonadeyinka@hotmail.co.uk>
2020-01-13 15:08:45 +0000

    changed the prize draw

--- service_4/application/routes.py
+++ service_4/application/routes.py
@@ -18,27 +18,44 @@ def result():
     points = int(point) * int(roll)
 
     if points <= 78:
-        prize_num = random.randint(1, 9)
-        query = Prize_gen.query.filter_by(id=prize_num).first()
+        query = Prize_gen.filter_by(id=1).first()
         prize = query.Prize
-
     elif points >= 79 and points <= 156:
-        prize_num = random.randint(10, 19)
-        query = Prize_gen.query.filter_by(id=prize_num).first()
+        query = Prize_get.filter_by(id=10).first()
         prize = query.Prize
-
-    elif points >= 157 and points <= 234:
-        prize_num = random.randint(20, 33)
-        query = Prize_gen.query.filter_by(id=prize_num).first()
+    elif points => 157 and points <= 234:
+        query = Prize_get.filter_by(id=20).first()
         prize = query.Prize
-
-    elif points >= 235 and points <= 317:
-        prize_num = random.randint(34, 39)
-        query = Prize_gen.query.filter_by(id=prize_num).first()
-        prize = prize = query.Prize
-
+    elif points => 235 and points <= 317:
+        query = Prize_get.filter_by(id=34).first()
+        prize = query.Prize
     elif points == 318:
-        query = Prize_gen.query.filter_by(id=40).first()
+        query = Prize_get.filter_by(id=40).first()
         prize = query.Prize
+
+
+    #if points <= 78:
+        #prize_num = random.randint(1, 9)
+        #query = Prize_gen.query.filter_by(id=prize_num).first()
+        #prize = query.Prize
+
+    #elif points >= 79 and points <= 156:
+        #prize_num = random.randint(10, 19)
+        #query = Prize_gen.query.filter_by(id=prize_num).first()
+        #prize = query.Prize
+
+    #elif points >= 157 and points <= 234:
+        #prize_num = random.randint(20, 33)
+        #query = Prize_gen.query.filter_by(id=prize_num).first()
+        #prize = query.Prize
+
+    #elif points >= 235 and points <= 317:
+        #prize_num = random.randint(34, 39)
+        #query = Prize_gen.query.filter_by(id=prize_num).first()
+        #prize = prize = query.Prize
+
+    #elif points == 318:
+        #query = Prize_gen.query.filter_by(id=40).first()
+        #prize = query.Prize
 
     return {'prize':'{}'.format(prize)}
