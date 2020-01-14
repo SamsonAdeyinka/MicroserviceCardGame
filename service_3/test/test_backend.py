9d2b90e2c58ebaa49e71f3022022d50504b5d56d
SamsonAdeyinka <samsonadeyinka@hotmail.co.uk>
2020-01-14 02:51:22 +0000

    test

--- service_3/test/test_backend.py
+++ service_3/test/test_backend.py
@@ -8,10 +8,10 @@ class TestBase(TestCase):
         config_name = "testing"
         return app
 
-
-def test_page(self):
-    response = self.client.get(url_for('service_3'))
-    self.assertEqual(response.status_code, 200)
+class TestService_3(TestBase):
+    def test_page(self):
+        response = self.client.get(url_for('/service_3'))
+        self.assertEqual(response.status_code, 200)
 
 # def test_roll_dice():
 #     assert roll_dice() == {'roll': '1'}
